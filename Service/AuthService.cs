ddde8efc8c1643974a52c2ed88b75b46a63a7d63
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2025-03-05 10:42:48 +0530

    Create AuthService.cs

---
+++ Service/AuthService.cs
@@ -0,0 +1,37 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CreateWebAPI.Sevices
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _config;
+
+        public AuthService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateJwtToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, username)
+        };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
